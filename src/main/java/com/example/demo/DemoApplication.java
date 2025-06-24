5c45b29672296160a3519f55b0e827af89227827
srikanth <sreekanth.rayapalli@gmail.com>
2025-06-24 16:25:52 +0530

    this for login validation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("this is sample");
+//this is sameple test for git
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
